66d9491a7999dba230f4e56a524f65bdf765a3ec
Feldy Yusuf <f3ldy10rpl@gmail.com>
2017-02-12 21:41:03 +0700

    implement feature

---
+++ form/admin/admin_list_artikel.php
@@ -0,0 +1,58 @@
+
+<div class="ibox float-e-margins">
+    <div class="ibox-title">
+        <h5>Daftar Artikel</h5>
+        <div class="ibox-tools">
+            <a class="collapse-link">
+                <i class="fa fa-chevron-up"></i>
+            </a>
+        </div>
+    </div>
+    <div class="ibox-content">
+        <div class="row">
+            <div class="col-sm-9">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-sm btn-primary"> Buat Artikel</button> 
+                </span>
+            </div>
+            <div class="col-sm-3">
+                <form method="post" action="">
+                    <div class="input-group">
+                        <input type="text" name="src_search" value="" placeholder="Pencarian" class="input-sm form-control" /> 
+                        <span class="input-group-btn">
+                            <button type="button" class="btn btn-sm btn-primary"> Go!</button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <table class="table table-hover no-margins">
+                <tbody>
+                <?php 
+                    $sql = mysqli_query($conn, "SELECT * FROM tbl_artikel order by tanggal desc");
+                    while($arr = mysqli_fetch_array($sql)) {
+                ?>
+                <tr>
+                    <td class="project-status">
+                        <span class="label label-warning"><?php echo $arr['category']; ?></span>
+                        <br/>
+                        <small><i class="fa fa-clock-o"></i> <?php echo get_tanggal($arr['tanggal']);?></small>
+                    </td>
+                    <td class="project-title">
+                        <a href="#"><?php echo $arr['judul']; ?></a>
+                        <br/>
+                        <small><?php echo trim_text(htmlspecialchars_decode($arr['isi']), 150);?></small>
+                    </td>
+                    <td class="project-actions">
+                        <a class="btn btn-white btn-sm"><i class="fa fa-folder"></i> View</a>
+                        <a class="btn btn-white btn-sm"><i class="fa fa-ban"></i> Edit</a>
+                        <a class="btn btn-white btn-sm"><i class="fa fa-send"></i> Publish</a>
+                    </td>
+                </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
--- form/admin/index.php
+++ form/admin/index.php
@@ -1,69 +1,88 @@
-<?php 
-    session_start();
-    if (empty($_SESSION['username']) || empty($_SESSION['password'])) {
-        echo "<script>window.location.href='form/login.php'</script>";
-    } else {
-?>
+<?php include("../../config/configuration.php");?>
 <!DOCTYPE html>
 <html>
-    <head>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
 
-        <title>Dashboard Admin</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link href="../../lib/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../../lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <title>Admin Dispusip</title>
 
-        <link href="../../lib/css/animate.css" rel="stylesheet">
-        <link href="../../css/default.css" rel="stylesheet">
-        <link href="../../lib/css/style.css" rel="stylesheet">
-        
-        <link href="../../lib/css/plugins/summernote/summernote.css" rel="stylesheet">
-        <link href="../../lib/css/plugins/summernote/summernote-bs3.css" rel="stylesheet">
-        <link href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet">
+    <link href="../../lib/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="../../lib/css/style.css" rel="stylesheet">
 
+</head>
 
-         <!-- Mainly scripts -->
-        <script src="../../lib/js/jquery-2.1.1.js"></script>
-        <script src="../../lib/js/bootstrap.min.js"></script>
-        <script src="../../lib/js/jquery-ui.js"></script>
-        
-        <!-- SUMMERNOTE -->
-        <script src="../../lib/js/plugins/summernote/summernote.min.js"></script>
-        <!-- Main JS -->
-        <script src="../../js/main-admin.js"></script>
-        <!-- Flot -->
-        <script src="../../lib/js/plugins/flot/jquery.flot.js"></script>
-        <script src="../../lib/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-        <script src="../../lib/js/plugins/flot/jquery.flot.spline.js"></script>
-        <script src="../../lib/js/plugins/flot/jquery.flot.resize.js"></script>
-        <script src="../../lib/js/plugins/flot/jquery.flot.pie.js"></script>
-        <script src="../../lib/js/plugins/flot/jquery.flot.symbol.js"></script>
-        <script src="../../lib/js/plugins/flot/jquery.flot.time.js"></script>
-        <!-- Chart -->
-        <script src="../../lib/js/plugins/chartJs/Chart.min.js"></script>
+<body style="background-color: #2f4050;">
+    <div id="wrapper">
+        <nav class="navbar-default navbar-static-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li class="nav-header">
+                        <div class="dropdown profile-element"> 
+                            <!-- <span><img alt="image" class="img-circle" src="img/profile_small.jpg" /></span> -->
+                            <span class="clear"> 
+                                <span class="block m-t-xs"> 
+                                    <strong class="font-bold">Nanang Suryana</strong>
+                             </span> <span class="text-muted text-xs block">Web Director</span> </span> 
+                        </div>
+                        <div class="logo-element">
+                            DS
+                        </div>
+                    </li>
+                    <li class="active">
+                        <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Menu</span> <span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li class="active"><a href="#">Content Artikel</a></li>
+                           <!--  <li ><a href="#">Dashboard v.2</a></li>
+                            <li ><a href="#">Dashboard v.3</a></li>
+                            <li ><a href="#">Dashboard v.4</a></li> -->
+                        </ul>
+                    </li>
+                </ul>
 
+            </div>
+        </nav>
 
-    </head>
-    <body >
-        <?php include("../../config/configuration.php");?>
-	    <?php 
-            $role = $_SESSION['role'];
-            if ($role == "admin") {
-                include("admin.php");
-            } 
-            else if ($role == "team") {}
-            else if ($role == "manager") {
-                include("dashboard.php");
-            }
-            else if ($role == "noc") {
-                include("noc.php");
-            } else {
-                echo "<script>window.location.href='form/login.php'</script>";
-            }
-	    ?>	
-	</body>
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+            <div class="row border-bottom">
+                <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+                    <div class="navbar-header">
+                        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+                    </div>
+                    <ul class="nav navbar-top-links navbar-right">
+                        <li><a href="#"><i class="fa fa-sign-out"></i> Log out</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="row  border-bottom white-bg dashboard-header">
+                <div class="col-lg-12">
+                    <h2>Welcome ${USER}</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12" style="padding: 0px;">
+                    <?php include("admin_list_artikel.php");?>
+                </div>
+            </div>
+            <div class="footer">
+                <div>
+                    <strong>Copyright</strong> Dispusip DKI Jakarta &copy; 2017
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="../../lib/js/jquery-2.1.1.js"></script>
+    <script src="../../lib/js/bootstrap.min.js"></script>
+    <script src="../../lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="../../lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- Custom and plugin javascript -->
+    <script src="../../lib/js/inspinia.js"></script>
+
+   
+</body>
 </html>
-<?php } ?>
